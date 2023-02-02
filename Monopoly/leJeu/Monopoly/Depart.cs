5f25cef8e4b5dc6d58e7e667ce216f68c25c3d3f
jessim <>
2023-02-02 11:04:03 +0100

    ajout de la classe ville et depart

---
+++ Monopoly/leJeu/Monopoly/Depart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetmonop
+{
+    public class Depart
+    {
+        private float rapportArret = 0;
+        private float rapportPassage = 0;
+
+        public float RapportArret { get => rapportArret; set => rapportArret = value; }
+        public float RapportPassage { get => rapportPassage; set => rapportPassage = value; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Monopoly/leJeu/Monopoly/Ville.cs
@@ -0,0 +1,35 @@
+﻿using Monopoly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetmonop
+{
+    public class Ville
+    {
+        private float prixAchat = 0;
+        private float prixNuitee = 0;
+        private string nom = "";
+        private Joueur proprietaire;
+
+        public float PrixAchat { get => prixAchat; set => prixAchat = value; }
+        public float PrixNuitee { get => prixNuitee; set => prixNuitee = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public Joueur Proprietaire { get => proprietaire; set => proprietaire = value; }
+
+        public Ville(int emplacement, float prixAchat, float prixNuitee, string nom, string libre)
+        {
+            this.prixAchat = prixAchat;
+            this.prixNuitee = prixNuitee;
+            this.nom = nom;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
+
